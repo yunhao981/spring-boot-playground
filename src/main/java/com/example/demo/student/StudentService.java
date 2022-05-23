96a6856528ab530276552127d1cc4b6bdb92f011
Yunhao Liu <yunhao981@hotmail.com>
2022-05-23 20:21:14 +0800

    updated find function

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -18,6 +19,11 @@ public class StudentService {
     }
 
     public void addNewStudent(Student student) {
+        Optional<Student> studentOptional = studentRepository.findStudentByEmail(student.getEmail());
+        if (studentOptional.isPresent()) {
+            throw new IllegalStateException("email taken");
+        }
+        studentRepository.save(student);
         System.out.println(student);
     }
 }
